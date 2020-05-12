134e423279ac8c35a5083b4e260875be922f532b
jaskirat1313 <jaskiratsingh568@gmail.com>
2020-05-12 16:25:05 +0530

    first assignment

---
+++ app.js
@@ -0,0 +1,30 @@
+(function () {
+    'use strict';
+    angular.module('LunchCheck',[]).
+    controller('LunchCheckController', LunchCheckController);
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope) {
+      $scope.lunchCheck = function(){
+                            var lunchMenu = $scope.lunchMenu;
+                            if(lunchMenu!=undefined && lunchMenu!=''){
+                              if(countElement(lunchMenu)<=3)
+                                $scope.message = 'Enjoy!';
+                              else
+                                $scope.message = 'Too much!';
+                              $scope.colors='green';
+                            }else{
+                              $scope.message = 'Please enter data first';
+                              $scope.colors='red';
+                            }
+                          };
+    }
+    function countElement(string){
+      var count = 0;
+      var list = string.split(',');
+      for(var i=0;i<list.length;i++){
+        if(list[i].trim()!='')
+          count++;
+      };
+      return count;
+    }
+  })();
